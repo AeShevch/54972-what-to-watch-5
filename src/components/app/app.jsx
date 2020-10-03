6eec50e98ad62248929b83ede8dea8a340e2ecd5
AeShevch <shevch.ae@gmail.com>
2020-10-03 22:32:32 +0300

    Начинаем программировать

---
+++ src/components/app/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "../main-page/main-page";
+
+const App = () => {
+  return (
+    <MainPage
+      title={`The Grand Budapest Hotel`}
+      genre={`Drama`}
+      date={`2014`}
+    />
+  );
+};
+
+export default App;
