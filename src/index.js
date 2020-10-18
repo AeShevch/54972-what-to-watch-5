3c3ce983f773d3eaca62f4424710c505334afa69
ae.shevch <shevch.ae@gmail.com>
2020-10-18 20:39:29 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import movies from "./mocks/films";
 import reviews from "./mocks/reviews";
 
-ReactDom.render(
-  <App movies={movies} reviews={reviews}/>,
-  document.getElementById(`root`),
+ReactDOM.render(
+    <App
+      movies={movies}
+      reviews={reviews}
+    />,
+    document.getElementById(`root`)
 );
