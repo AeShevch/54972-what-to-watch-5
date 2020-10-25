647e9e69c7c98622c47c3cbbcf9196bad77d12f0
AeShevch <shevch.ae@gmail.com>
2020-10-25 14:49:39 +0300

    refactor: Выносит магические числа в константу, добавляет очистку таймера перед размонтированием компонента, выносит создание таймаута в отдельный метод

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import SmallMovieCard from "../small-movie-card/small-movie-card";
 import moviesListProp from "../small-movie-card/small-movie-card.prop";
 
+const EMPTY_INDEX = -1;
+const MS_DELAY_TO_PLAY_VIDEO = 1000;
+
 export default class MoviesList extends PureComponent {
   constructor(props) {
     super(props);
@@ -10,28 +13,35 @@ export default class MoviesList extends PureComponent {
     this._movieCardMouseLeaveHandle = this._movieCardMouseLeaveHandle.bind(this);
     this._movieCardMouseOverHandle = this._movieCardMouseOverHandle.bind(this);
 
-    this.state = {
-      activeMovieIndex: `-1`,
+    this._defaultState = {
+      activeMovieIndex: EMPTY_INDEX,
     };
 
+    this.state = this._defaultState;
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this._timeout);
+  }
+
+  _getTimeoutToPlayVideo(movieCardIndex) {
+    return setTimeout(
+        () => {
+          this.setState({
+            activeMovieIndex: movieCardIndex,
+          });
+        }, MS_DELAY_TO_PLAY_VIDEO);
   }
 
   _movieCardMouseLeaveHandle() {
-    this.setState({
-      activeMovieIndex: `-1`,
-    });
+    this.setState(this._defaultState);
     clearTimeout(this._timeout);
   }
 
   _movieCardMouseOverHandle(evt) {
     evt.persist();
     const currentIndex = +evt.currentTarget.dataset.cardIndex;
-    this._timeout = setTimeout(
-        () => {
-          this.setState({
-            activeMovieIndex: currentIndex,
-          });
-        }, 1000);
+    this._timeout = this._getTimeoutToPlayVideo(currentIndex);
   }
 
   render() {
